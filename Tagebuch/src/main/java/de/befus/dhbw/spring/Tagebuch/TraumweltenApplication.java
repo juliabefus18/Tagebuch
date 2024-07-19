b67832d4d2eb304c40bbb2ef9d1978e5ad41cb62
juliabefus18 <115024236+juliabefus18@users.noreply.github.com>
2024-07-19 11:12:37 +0200

    ,kmjciohd

---
+++ Tagebuch/src/main/java/de/befus/dhbw/spring/Tagebuch/TraumweltenApplication.java
@@ -0,0 +1,13 @@
+package de.befus.dhbw.spring.Tagebuch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TraumweltenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TraumweltenApplication.class, args);
+	}
+
+}
---
+++ Tagebuch/src/test/java/de/befus/dhbw/spring/Tagebuch/TraumweltenApplicationTests.java
@@ -0,0 +1,13 @@
+package de.befus.dhbw.spring.Tagebuch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraumweltenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
